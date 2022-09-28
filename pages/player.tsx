3ebd105ee5faa426a64962065402c7634541cd89
Alexander Dmitriev <dmitriealeksan@yandex.ru>
2022-09-28 19:07:52 +0300

    Player 1

---
+++ pages/player.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MediaControlCard from '../src/components/MediaControlCard'
+
+function Player() {
+  return (
+    <div>
+        <MediaControlCard />
+    </div>
+  )
+}
+
+export default Player
